aefb9cfa597e5483853c4131d43c67d0648d1fd6
Ilya Kukharchuk <bartash12345@gmail.com>
2025-07-18 17:09:35 +0300

    fix ApplicationAdministrationTest

--- src/test/java/admin/ApplicationAdministrationTest.java
+++ src/test/java/admin/ApplicationAdministrationTest.java
@@ -4,16 +4,22 @@ import by.louegroove.seleniumfreecodecamp.pages.adminPage.ApplicationAdministrat
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class ApplicationAdministrationTest extends AdminTest {
+    WebDriverWait wait;
     private ApplicationAdministrationPage applicationAdministrationPage;
+    private int count = 0;
     @BeforeClass
     public void loginAsAdmin() throws InterruptedException {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         applicationAdministrationPage = registrationDetailsPage.insertRegDetails(
                 "Иванов",
                 "Иван",
@@ -25,19 +31,24 @@ public class ApplicationAdministrationTest extends AdminTest {
     }
     @BeforeMethod
     public void enterToAdminTablePage() {
-        homePage.clickEnterAsAdminButton();
+        count++;
+        if (count > 1){
+            System.out.println("clickEnterAsAdminButton");
+            homePage.clickEnterAsAdminButton();
+        }
     }
 
     @AfterMethod
     public void returnToHomePage() {
         if (applicationAdministrationPage != null) {
+            System.out.println("clickCloseButton");
             applicationAdministrationPage.clickCloseButton();
-            applicationAdministrationPage = null; // Очищаем ссылку
         }
     }
 
     @Test
     public void testPresenceOfApplicationTable() throws InterruptedException {
+        System.out.println("testPresenceOfApplicationTable");
 
         wait.until(ExpectedConditions.visibilityOf(applicationAdministrationPage.getApplicationsTable()));
         Assert.assertTrue(applicationAdministrationPage.isApplicationsTableDisplayed(),
@@ -46,6 +57,7 @@ public class ApplicationAdministrationTest extends AdminTest {
 
     @Test
     public void testLikeButton() throws InterruptedException {
+        System.out.println("testLikeButton");
         wait.until(ExpectedConditions.elementToBeClickable(applicationAdministrationPage.getLikeButton()));
         applicationAdministrationPage.clickLikeButton();
         wait.until(ExpectedConditions.textToBePresentInElement(
@@ -55,6 +67,7 @@ public class ApplicationAdministrationTest extends AdminTest {
 
     @Test
     public void testDislikeButton() throws InterruptedException {
+        System.out.println("testDislikeButton");
         wait.until(ExpectedConditions.elementToBeClickable(applicationAdministrationPage.getDislikeButton()));
         applicationAdministrationPage.clickDislikeButton();
         wait.until(ExpectedConditions.textToBePresentInElement(
