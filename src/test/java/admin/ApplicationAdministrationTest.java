38786f4cdf489b2b3bfd6ed5f491d1c0f09c769f
Ilya Kukharchuk <bartash12345@gmail.com>
2025-07-22 14:06:22 +0300

    implement log4j

--- src/test/java/admin/ApplicationAdministrationTest.java
+++ src/test/java/admin/ApplicationAdministrationTest.java
@@ -1,6 +1,8 @@
 package admin;
 
 import by.louegroove.seleniumfreecodecamp.pages.adminPage.ApplicationAdministrationPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -9,10 +11,14 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ApplicationAdministrationTest extends AdminTest {
+
+    private static final Logger logger = LogManager.getLogger(ApplicationAdministrationTest.class);
+
     private ApplicationAdministrationPage applicationAdministrationPage;
 
     @BeforeClass
     public void loginAsAdmin() {
+        logger.info("Logging in as admin...");
         applicationAdministrationPage = registrationDetailsPage.insertRegDetails(
                 "Иванов",
                 "Иван",
@@ -21,9 +27,12 @@ public class ApplicationAdministrationTest extends AdminTest {
                 "12345678",
                 "22111998"
         );
+        logger.info("Successfully logged in as admin.");
     }
+
     @BeforeMethod
     public void enterToAdminTablePage() {
+        logger.info("Entering admin table page...");
         if (!skipInitialAdminButtonClick) {
             homePage.clickEnterAsAdminButton();
         }
@@ -32,6 +41,7 @@ public class ApplicationAdministrationTest extends AdminTest {
 
     @AfterMethod
     public void returnToHomePage() {
+        logger.info("Returning to home page...");
         if (applicationAdministrationPage != null) {
             applicationAdministrationPage.clickCloseButton();
         }
@@ -39,23 +49,29 @@ public class ApplicationAdministrationTest extends AdminTest {
 
     @Test
     public void testPresenceOfApplicationTable() {
+        logger.info("Testing presence of application table...");
         Assert.assertTrue(applicationAdministrationPage.isApplicationsTableDisplayed(),
                 "Таблица заявок должна быть видимой");
+        logger.info("Application table is visible.");
     }
 
     @Test
-    public void testLikeButton(){
+    public void testLikeButton() {
+        logger.info("Testing like button...");
         applicationAdministrationPage.clickLikeButton();
         wait.until(ExpectedConditions.textToBePresentInElement(
                 applicationAdministrationPage.getApplicationStatus(), "Одобрена"));
         Assert.assertEquals(applicationAdministrationPage.getStatus(), "Одобрена");
+        logger.info("Like button test passed.");
     }
 
     @Test
     public void testDislikeButton() {
+        logger.info("Testing dislike button...");
         applicationAdministrationPage.clickDislikeButton();
         wait.until(ExpectedConditions.textToBePresentInElement(
                 applicationAdministrationPage.getApplicationStatus(), "Отклонена"));
         Assert.assertEquals(applicationAdministrationPage.getStatus(), "Отклонена");
+        logger.info("Dislike button test passed.");
     }
 }
